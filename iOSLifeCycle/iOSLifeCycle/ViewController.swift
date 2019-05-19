168e344d25eb3d0e0632d86ba85f2d90e542f9d5
Piyawat Aramsri <zzon_na@hotmail.com>
2019-05-19 14:14:01 +0700

    init project

---
+++ iOSLifeCycle/iOSLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLifeCycle
+//
+//  Created by Uuuu on 19/5/2562 BE.
+//  Copyright © 2562 IceCreamCremo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
